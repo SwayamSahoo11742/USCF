c859405833c1d09b5e7194bfae273d3ad9e2f060
lampham468 <lampham468@berkeley.edu>
2023-06-12 00:56:33 -0700

    actually implement reddit analysis

--- reddit/reddit_analysis.py
+++ reddit/reddit_analysis.py
@@ -1,20 +1,12 @@
-import datetime
 import json
 import os
+from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 
 import praw
 import textblob
 
-# Save number of mentions on SupaBase
-# Save sentiment for each day for the past 5 days on SupaBase
-"""
-data = {
-    "date": now.strftime("%Y-%m-%d"),
-    "keyword": keyword,
-    "mentions": count
-}
-"""
 
 def create_reddit_instance():
     return praw.Reddit(
@@ -24,33 +16,75 @@ def create_reddit_instance():
     )
 
 
-def store_data(data):
+# TODO: Save data to Supabase
+def store(data):
     pass
 
-def analyze(reddit, target, subreddit="all", time_filter="hour"):
 
-    num_mentions = 0
-    sentiments = []
-    def process_comment(comment):
-        nonlocal num_mentions
+def convert_time(utc_time):
+    return datetime.fromtimestamp(utc_time).replace(minute=0, second=0, microsecond=0).isoformat()
 
-        text = comment.body
-        num_mentions += text.count(target)
 
-        if num_mentions == 0: return
+def count_mentions(text, target):
+    return text.count(target)
 
-        polarity, _ = textblob.TextBlob(text).sentiment
-        polarity *= 100
 
-        sentiments.append({
-            "date": datetime.now().strftime("%Y-%m-%d"),
-            "keyword": keyword,
-            "sentiment": polarity
-        })
+def compute_sentiment_score(text):
+    polarity, _ = textblob.TextBlob(text).sentiment
+    return polarity
 
-    for submission in reddit.subreddit(subreddit).search(target, time_filter=time_filter, limit=None):
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
-            process_comment(comments)
 
-    return num_mentions, sentiments
+def process_and_store_data(dt, text, target, mentions, sentiments):
+    num_mentions = count_mentions(text, target)
+
+    if num_mentions == 0: return
+
+    mentions[dt] += num_mentions
+
+    sentiment_score = compute_sentiment_score(text)
+
+    total_sentiment_score, entry_count = sentiments[dt]
+    sentiments[dt] = (total_sentiment_score + sentiment_score, entry_count + 1)
+
+
+def process_comment(comment, target, mentions, sentiments):
+    dt = convert_time(comment.created_utc)
+    text = comment.body.lower()
+    process_and_store_data(dt, text, target, mentions, sentiments)
+
+
+def process_submission(submission, target, mentions, sentiments):
+    # Process the title and text of the main post.
+    dt = convert_time(submission.created_utc)
+    text = f"{submission.title}\n\n{submission.selftext}".lower()
+    process_and_store_data(dt, text, target, mentions, sentiments)
+    
+    # Process the comments and nested replies.
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        process_comment(comment, target, mentions, sentiments)
+
+
+def analyze(reddit, target, time_filter="hour"):
+    mentions = defaultdict(int)
+    sentiments = defaultdict(lambda: (0, 0))
+
+    for submission in reddit.subreddit("all").search(target, time_filter=time_filter, limit=None):
+        # print(f"https://www.reddit.com/{submission.permalink}")
+        process_submission(submission, target, mentions, sentiments)
+
+    # Compute averages.
+    sentiments = {k : (total_sentiment_score / entry_count) 
+                  for k, (total_sentiment_score, entry_count) in sentiments.items()}
+
+    return mentions, sentiments
+
+
+def main():
+    target = "facebook" # replace with command-line arg or whatever
+    reddit = create_reddit_instance()
+    num_mentions, sentiments = analyze(reddit, target)
+
+
+# if __name__ == "__main__":
+#     main()
